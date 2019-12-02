81b6f72fdf479ef5bf9065d3cd6a4e0e57278b7e
pix <tpxsky@163.com>
2019-12-03 00:29:42 +0800

    add templates test

--- templates-demo/app.py
+++ templates-demo/app.py
@@ -1,13 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
 # templates test demo
-@app.route('/')
-@app.route('/<name>')
+@app.route('/',methods=['GET','POST'])
+@app.route('/<name>',methods=['GET','POST'])
 def hello_world(name=None):
     return render_template('hello.html',name=name)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+with app.test_request_context('/',method='POST'):
+    assert request.method == 'GET'
+    assert request.path == '/hello'
