fa257414df39b764bf8bfd70731a456f114b4ee0
pix <tpxsky@163.com>
2019-12-03 15:32:15 +0800

    add Class List Json test

--- json-response-demo/app.py
+++ json-response-demo/app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask,jsonify
+from json import JSONEncoder
 
 app = Flask(__name__)
 
+class Student:
+    age = 0
+    name=""
+    def __init__(self,age,name):
+        self.age = age
+        self.name = name
+
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+def get_all_users():
+    user_list = []
+    user_list.append(Student(11,"xiaoming"))
+    user_list.append(Student(22,"Jams"))
+    user_list.append(Student(33,"Anthony"))
+    user_list.append(Student(44,"Bosh"))
+    return user_list
 
 @app.route('/')
 def hello_world():
@@ -16,7 +35,13 @@ def python_resp():
         "nullobjj":None
     }
 
-# TODO:response users api,use jsonify
+# response users api,use jsonify
+@app.route('/users/')
+def users_api():
+    users = get_all_users()
+
+    # return jsonify(json.dump(users))
+    return MyEncoder().encode(users)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
