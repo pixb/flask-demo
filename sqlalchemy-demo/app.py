03a0afe606d887d71f6b003135b7593a4a3913df
pix <tpxsky@163.com>
2019-12-14 16:49:05 +0800

    修改flask demo

--- sqlalchemy-demo/app.py
+++ sqlalchemy-demo/app.py
@@ -24,19 +24,24 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}'.format(self.username)
-
+@app.route('/')
 def hello_world():
     return 'Hello World!'
 
 @app.route('/adduser/')
 def add_user():
-    db.session.add(User("zhangsan"))
-    db.session.add(User("scott"))
-    db.session.add(User("Jams"))
-    db.session.add(User("Wade"))
-    db.session.add(User("hart"))
-    db.session.add(User("marin"))
-    db.session.add(User("thethy"))
+    admin_role = Role(name='Admin')
+    mod_role = Role(name='Moderator')
+    user_role = Role(name='User')
+    user_john = User(username='john',role=admin_role)
+    user_susan = User(username='susan',role=user_role)
+    user_david = User(username='david',role=user_role)
+    db.session.add(admin_role)
+    db.session.add(mod_role)
+    db.session.add(user_role)
+    db.session.add(user_john)
+    db.session.add(user_susan)
+    db.session.add(user_david)
     db.session.commit()
     return "add all user success!"
 
