c5e3c3b4b96c38e0f8509adb0f7d2056f30f891f
pix <tpxsky@163.com>
2019-12-16 03:34:31 +0800

    reslove error

--- flask-project-struct/config.py
+++ flask-project-struct/config.py
@@ -8,6 +8,8 @@ class Config:
     # 程序密钥，从环境变量中读取
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    DEBUG=True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
