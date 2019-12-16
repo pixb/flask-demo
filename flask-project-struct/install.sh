1e52499c59ed50ff555e5877bc2163f53af589eb
pix <tpxsky@163.com>
2019-12-16 13:50:27 +0800

    modify python version to 3.8.0

--- flask-project-struct/install.sh
+++ flask-project-struct/install.sh
@@ -1,6 +1,6 @@
 pip3 install flask-bootstrap
 pip3 install flask-mail
-pip3 install flask-momene
+pip3 install flask-moment
 pip3 install flask-sqlalchemy
 pip3 install flask-script
 pip3 install flask-migrate
