e92f4efa5532faf006de749f1db8941a9c00eac7
pix <tpxsky@163.com>
2019-12-24 07:19:21 +0800

    add moment test

--- flask-bootstrap-templates/app.py
+++ flask-bootstrap-templates/app.py
@@ -1,20 +1,22 @@
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
 
 @app.errorhandler(404)
 def page_not_found(e):
